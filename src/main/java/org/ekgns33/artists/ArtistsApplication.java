abe1e4a74cbe602bd7267bc7e13fa68e1f37f0e1
ekgns33 <ekgns00421@gmail.com>
2025-05-30 14:10:11 +0900

    init

---
+++ src/main/java/org/ekgns33/artists/ArtistsApplication.java
@@ -0,0 +1,13 @@
+package org.ekgns33.artists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtistsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ekgns33/artists/ArtistsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ekgns33.artists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
